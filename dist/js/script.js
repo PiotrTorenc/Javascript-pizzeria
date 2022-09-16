eea17fb24db64e0298cdac851e4df4805d7d9b8c
PiotrTorenc <piotr.torenc@gmail.com>
2022-09-17 00:54:52 +0200

    render products

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,14 +55,36 @@
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
-
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log("new Product", thisProduct);
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
   const app = {
+    initMenu: function () {
+      const thisApp = this;
+      console.log("thisApp.data:", thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function () {
       const thisApp = this;
       console.log("*** App starting ***");
@@ -70,6 +92,8 @@
       console.log("classNames:", classNames);
       console.log("settings:", settings);
       console.log("templates:", templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
+const { render } = require("sass");
+
 {
   ("use strict");
 
@@ -55,14 +57,32 @@
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
-
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log("new Product", thisProduct);
+    }
+    renderInMenu() {
+      const thisProduct = this;
     }
   }
 
   const app = {
+    initMenu: function () {
+      const thisApp = this;
+      console.log("thisApp.data:", thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function () {
       const thisApp = this;
       console.log("*** App starting ***");
@@ -70,6 +90,8 @@
       console.log("classNames:", classNames);
       console.log("settings:", settings);
       console.log("templates:", templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
