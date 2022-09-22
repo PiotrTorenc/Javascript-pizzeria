94873b9d8b8a7b7735ce42a4993481a963bae41a
PiotrTorenc <piotr.torenc@gmail.com>
2022-09-22 17:29:14 +0200

    add AmountWidget

--- dist/js/script.js
+++ dist/js/script.js
@@ -64,9 +64,8 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
-
-      console.log("new product:", thisProduct);
     }
 
     renderInMenu() {
@@ -102,6 +101,13 @@
       thisProduct.imageWrapper = thisProduct.element.querySelector(
         select.menuProduct.imageWrapper
       );
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(
+        select.menuProduct.amountWidget
+      );
+    }
+    initAmountWidget() {
+      const thisProduct = this;
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
     }
 
     initAccordion() {
@@ -124,16 +130,13 @@
           activeProduct !== null && activeProduct !== thisProduct.element
             ? activeProduct.classList.remove("active")
             : thisProduct.element;
-          console.log("class active removed:", activeProduct);
         }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle("active");
-        console.log("toggle class:", thisProduct.element);
       });
     }
     initOrderForm() {
       const thisProduct = this;
-      // console.log('initOrderForm');
 
       thisProduct.form.addEventListener("submit", function (event) {
         event.preventDefault();
@@ -155,20 +158,16 @@
 
     processOrder() {
       const thisProduct = this;
-      console.log("processOrder");
 
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log("formData", formData);
 
       let price = thisProduct.data.price;
 
       for (let paramId in thisProduct.data.params) {
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
 
         for (let optionId in param.options) {
           const option = param.options[optionId];
-          console.log(optionId, option);
 
           const optionSelected =
             formData[paramId] && formData[paramId].includes(optionId);
@@ -196,11 +195,32 @@
     }
   }
 
+  class AmountWidget {
+    constructor(element) {
+      const thisWidget = this;
+      thisWidget.getElements(element);
+      console.log("AmountWidget:", thisWidget);
+      console.log("constructor arguments:", element);
+    }
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(
+        select.widgets.amount.input
+      );
+      thisWidget.linkDecrease = thisWidget.element.querySelector(
+        select.widgets.amount.linkDecrease
+      );
+      thisWidget.linkIncrease = thisWidget.element.querySelector(
+        select.widgets.amount.linkIncrease
+      );
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
-
-      console.log("thisApp.data:", thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
@@ -215,11 +235,6 @@
 
     init: function () {
       const thisApp = this;
-      console.log("*** App starting ***");
-      console.log("thisApp:", thisApp);
-      console.log("classNames:", classNames);
-      console.log("settings:", settings);
-      console.log("templates:", templates);
 
       thisApp.initData();
       thisApp.initMenu();
